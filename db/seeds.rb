f2304904e25ff0e9469e683a0470e1f04035641d
kylewelt <kylewelt@umich.edu>
2017-07-12 15:55:26 -0400

    edit seeds for future dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ u = User.create(first_name: 'Carl', last_name: 'Person', email: 'c@example.com',
 
 l = Location.create(long: 40.7128, lat: 74.0059)
 
-s = Sublet.create(name: "Cool Digz", owner: u, price: 4000, start_date: Date.parse('Apr 1, 2017'), end_date: Date.parse('Aug 8, 2017'), location: l)
+s = Sublet.create(name: "Cool Digz", owner: u, price: 4000, start_date: Date.parse('Aug 1, 2017'), end_date: Date.parse('Jan 1, 2018'), location: l)
 
 s.images << Image.create(url: 'https://images.unsplash.com/photo-1466098672325-c9ddda4b7975')
 
@@ -18,6 +18,6 @@ u2 = User.create(first_name: 'Second', last_name: 'People', email: 's@example.co
 
 l2 = Location.create(long: 49.7128, lat: 94.0059)
 
-s2 = Sublet.create(name: "Random Digz", owner: u2, price: 600, start_date: Date.parse('Jun 13, 2017'), end_date: Date.parse('Dec 28, 2017'), location: l2)
+s2 = Sublet.create(name: "Random Digz", owner: u2, price: 6000, start_date: Date.parse('Sep 1, 2017'), end_date: Date.parse('Sep 1, 2018'), location: l2)
 
 s2.images << Image.create(url: 'https://images.unsplash.com/photo-1466098672325-c9ddda4b7975')
