eaa7646a5b0aeec4c3ba321df528349a601c803e
kylewelt <kylewelt@umich.edu>
2017-07-14 10:59:33 -0400

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,105 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+desc = [
+  'Spacious, elegant, and majestic, this enormous apartment features marble counter tops, Viking stainless steel appliances, bamboo floors and cabinets, deluxe bathrooms, featuring standing glass showers and beautiful soaking tub options, walk-in closets, and sweeping views. Just as exquisite as it is practical, this gorgeous apartment is the epitome of real estate heaven!',
+  'This apartment combines light, quiet, and space in a way rarely found. Studio living at includes a boutique style unit with high ceilings, hardwood floors, and an oversize bathroom. State of the art appliances and custom cabinetry deliver a chic New York style. Most importantly, living here makes you a part of our community. Our amenities and tenant events turn your rental into a home.',
+  'Recently renovated, these well-lit units feature unique barnwood-style plank flooring and stainless steel appliances along with a balcony or private terrace in most units. Amenities include a newly renovated lobby with a seating area and fireplace, 24-hour doorman, redesigned top floor amenities including a state-of-the-art fitness center, residents lounge with billiards and catering kitchen, and an on-site garage. Laundry is located on every floor and in some select units, pets are welcome!',
+  'Condo quality finishes in this double exposure (south and west) alcove studio. With natural hardwood strip flooring, soaring ceilings and an open layout, this is where you want to come home. A full sized washer and dryer, top-of-the-line stainless steel appliances, including a dishwasher and microwave. The kitchen also has custom Italian satin finish cabinetry with soft-close features. The bathroom is surprisingly spacious with a deep soaking tub and built-in grooming station. This intelligently designed apartment has floor-to-ceiling windows and unbeatable closets. Will fit a king sized bed plus additional furniture.',
+  'Stunning & fully renovated large 3BR in the heart of Gramercy Park. A separate kitchen includes stainless steel appliances with new granite counter tops and cabinets. In addition to the kitchen, the bathroom is also completely gut renovated with marble tile, a new shower bath, and beautiful vanity. The bedrooms all fit a queen size beds! Enjoy recessed lighting throughout this unit. Walk to shopping, cafes, nightlife, and Gramercy Park! Washer/Dryer inside your apartment for Laundry! Newly renovated building.',
+  'Stunning & fully renovated large apartment. Elevator Building! Brand new kitchen includes all new high end modern appliances, custom marble counter tops, & white/grey cabinets. Sunny, Bright, & Spacious this apartment boasts a charming atmosphere with crown moldings and condo finishes. The bathroom is also completely gut renovated with marble tile, a new shower with euro glass, and beautiful vanity. The bedrooms; 1 fits a KING size bed and the other fits a queen size bed. The bedrooms also feature large closets, and there is another closet in the foyer. Enjoy recessed lighting throughout this unit.',
+  'NO FEE!!! OPEN CITY VIEWS - Flex two bedroom, one bathroom. 24 HR doorman, live in super, elevator - gorgeous building, close to shops, major transportation and parks. Large renovated kitchen with all new appliances. ample closet space, renovated bathroom, living room is facing 30th street -very bright and open . Large entry hallway. This is a must see apartment!',
+  'Enormous apartment with great sunlight! Elevator building! Huge apartment so big currently being used by 4 roommates! Flex 4BR or Big 3BR! Rents fast. Steps to cafes, restaurants, and best nightlife. '
+]
 
-text = "Spacious, elegant, and majestic, this enormous 2125 sqft apartment features marble counter tops, Viking stainless steel appliances, bamboo floors and cabinets, deluxe bathrooms, featuring standing glass showers and beautiful soaking tub options, walk-in closets, and sweeping views of the Hudson River. Just as exquisite as it is practical, this gorgeous apartment is the epitome of Manhattan real estate heaven!"
+br = [
+  'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+  'https://images.unsplash.com/photo-1466098672325-c9ddda4b7975',
+  'https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg',
+  'https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg',
+  'https://images.pexels.com/photos/90319/pexels-photo-90319.jpeg',
+  'https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg',
+  'https://images.pexels.com/photos/29683/pexels-photo-29683.jpg',
+  'https://images.pexels.com/photos/271695/pexels-photo-271695.jpeg'
+]
 
-u = User.create(first_name: 'Carl', last_name: 'Person', email: 'c@example.com', password: 'dragons')
+lr = [
+  'https://images.pexels.com/photos/243148/pexels-photo-243148.jpeg',
+  'https://images.pexels.com/photos/245208/pexels-photo-245208.jpeg',
+  'https://images.pexels.com/photos/265579/pexels-photo-265579.jpeg',
+  'https://images.pexels.com/photos/77931/pexels-photo-77931.jpeg',
+  'https://images.pexels.com/photos/279607/pexels-photo-279607.jpeg',
+  'https://images.pexels.com/photos/311398/pexels-photo-311398.jpeg',
+  'https://images.pexels.com/photos/299629/pexels-photo-299629.jpeg',
+  'https://images.pexels.com/photos/279719/pexels-photo-279719.jpeg'
+]
 
-l = Location.create(lat: 40.7128, long: -74.0059)
+kr = [
+  'https://images.pexels.com/photos/273822/pexels-photo-273822.jpeg',
+  'https://images.pexels.com/photos/273843/pexels-photo-273843.jpeg',
+  'https://images.pexels.com/photos/94865/pexels-photo-94865.jpeg',
+  'https://images.pexels.com/photos/259828/pexels-photo-259828.jpeg',
+  'https://images.pexels.com/photos/100575/pexels-photo-100575.jpeg',
+  'https://images.pexels.com/photos/265578/pexels-photo-265578.jpeg',
+  'https://images.pexels.com/photos/271762/pexels-photo-271762.jpeg',
+  'https://images.pexels.com/photos/276651/pexels-photo-276651.jpeg'
+]
 
-s = Sublet.create(name: "Cool Digz", owner: u, price: 4000, start_date: Date.parse('Aug 1, 2017'), end_date: Date.parse('Jan 1, 2018'), location: l, description: text)
+users = [
+  User.create(first_name: 'Adam', last_name: 'Adamsson', email: 'a@example.com', password: 'dragons'),
+  User.create(first_name: 'Brian', last_name: 'Briansson', email: 'b@example.com', password: 'dragons'),
+  User.create(first_name: 'Carl', last_name: 'Carlsson', email: 'c@example.com', password: 'dragons'),
+  User.create(first_name: 'Darren', last_name: 'Darrensson', email: 'd@example.com', password: 'dragons'),
+  User.create(first_name: 'Elrod', last_name: 'Elrodsson', email: 'e@example.com', password: 'dragons'),
+  User.create(first_name: 'Frank', last_name: 'Franksson', email: 'f@example.com', password: 'dragons'),
+  User.create(first_name: 'George', last_name: 'Georgesson', email: 'g@example.com', password: 'dragons'),
+  User.create(first_name: 'Harold', last_name: 'Haroldsson', email: 'h@example.com', password: 'dragons')
+]
 
-s.images << Image.create(url: 'https://images.unsplash.com/photo-1466098672325-c9ddda4b7975', caption: "room")
+loc = [
+  Location.create(lat: 40.7120, long: -74.0050),
+  Location.create(lat: 40.7121, long: -74.0051),
+  Location.create(lat: 40.7122, long: -74.0052),
+  Location.create(lat: 40.7123, long: -74.0053),
+  Location.create(lat: 40.7124, long: -74.0054),
+  Location.create(lat: 40.7125, long: -74.0055),
+  Location.create(lat: 40.7126, long: -74.0056),
+  Location.create(lat: 40.7127, long: -74.0057)
+]
 
-u2 = User.create(first_name: 'Second', last_name: 'People', email: 's@example.com', password: 'dragons')
+s0 = Sublet.create(name: "Cool Digz", owner: users[0], price: 80000, start_date: Date.parse('Aug 1, 2017'), end_date: Date.parse('Oct 1, 2017'), location: loc[0], description: desc[0])
+s0.images << Image.create(url: br[0], caption: "Bedroom")
+s0.images << Image.create(url: lr[0], caption: "Living Room")
+s0.images << Image.create(url: kr[0], caption: "Kitchen")
 
-l2 = Location.create(lat: 43.7128, long: -76.0059)
+s1 = Sublet.create(name: "Concrete Jungle", owner: users[1], price: 100000, start_date: Date.parse('Aug 1, 2017'), end_date: Date.parse('Nov 1, 2017'), location: loc[1], description: desc[1])
+s1.images << Image.create(url: br[1], caption: "Bedroom")
+s1.images << Image.create(url: lr[1], caption: "Living Room")
+s1.images << Image.create(url: kr[1], caption: "Kitchen")
 
-s2 = Sublet.create(name: "Random Digz", owner: u2, price: 6000, start_date: Date.parse('Sep 1, 2017'), end_date: Date.parse('Sep 1, 2018'), location: l2, description: text)
+s2 = Sublet.create(name: "Modern Living", owner: users[2], price: 60000, start_date: Date.parse('Aug 1, 2017'), end_date: Date.parse('Dec 1, 2017'), location: loc[2], description: desc[2])
+s2.images << Image.create(url: br[2], caption: "Bedroom")
+s2.images << Image.create(url: lr[2], caption: "Living Room")
+s2.images << Image.create(url: kr[2], caption: "Kitchen")
 
-s2.images << Image.create(url: 'https://images.unsplash.com/photo-1466098672325-c9ddda4b7975', caption: "room")
+s3 = Sublet.create(name: "Bougie AF", owner: users[3], price: 120000, start_date: Date.parse('Sep 1, 2017'), end_date: Date.parse('Oct 1, 2017'), location: loc[3], description: desc[3])
+s3.images << Image.create(url: br[3], caption: "Bedroom")
+s3.images << Image.create(url: lr[3], caption: "Living Room")
+s3.images << Image.create(url: kr[3], caption: "Kitchen")
+
+s4 = Sublet.create(name: "Party Down", owner: users[4], price: 95000, start_date: Date.parse('Sep 1, 2017'), end_date: Date.parse('Nov 1, 2017'), location: loc[4], description: desc[4])
+s4.images << Image.create(url: br[4], caption: "Bedroom")
+s4.images << Image.create(url: lr[4], caption: "Living Room")
+s4.images << Image.create(url: kr[4], caption: "Kitchen")
+
+s5 = Sublet.create(name: "Get Some", owner: users[5], price: 750000, start_date: Date.parse('Sep 1, 2017'), end_date: Date.parse('Dec 1, 2017'), location: loc[5], description: desc[5])
+s5.images << Image.create(url: br[5], caption: "Bedroom")
+s5.images << Image.create(url: lr[5], caption: "Living Room")
+s5.images << Image.create(url: kr[5], caption: "Kitchen")
+
+s6 = Sublet.create(name: "So Cool", owner: users[6], price: 95000, start_date: Date.parse('Oct 1, 2017'), end_date: Date.parse('Dec 1, 2017'), location: loc[6], description: desc[6])
+s6.images << Image.create(url: br[6], caption: "Bedroom")
+s6.images << Image.create(url: lr[6], caption: "Living Room")
+s6.images << Image.create(url: kr[6], caption: "Kitchen")
+
+s7 = Sublet.create(name: "Summer Fun", owner: users[7], price: 75000, start_date: Date.parse('Oct 1, 2017'), end_date: Date.parse('Jan 1, 2018'), location: loc[7], description: desc[7])
+s7.images << Image.create(url: br[7], caption: "Bedroom")
+s7.images << Image.create(url: lr[7], caption: "Living Room")
+s7.images << Image.create(url: kr[7], caption: "Kitchen")
